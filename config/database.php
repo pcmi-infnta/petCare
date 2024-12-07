f5d2f58ebeead1db801e83ce815edf366be18bed
pcmi <pcminfanta@gmail.com>
2024-12-07 15:11:57 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,16 +1,22 @@
-<?php
-define('DB_HOST', getenv('DB_HOST'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASS', getenv('DB_PASS'));
-define('DB_NAME', getenv('DB_NAME'));
-
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "/*.php",
+      "use": "vercel-php@0.6.0"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "/index.php"
+    }
+  ],
+  "env": {
+    "APP_ENV": "production",
+    "DB_HOST": "sql300.infinityfree.com",
+    "DB_NAME": "if0_37858321_petCareDB",
+    "DB_USER": "if0_37858321",
+    "DB_PASS": "GcEQFqnydWQU"
+  }
 }
-
-$conn->set_charset("utf8");
-
-return $conn;
-?>
